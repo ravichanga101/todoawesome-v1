16f90714e35935ba133cc5b81fd220b4057e3eb4
Ravi Patel <ravifromchanga@gmail.com>
2024-02-08 16:23:10 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -6,11 +6,10 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "build/index.html"));
-})
-
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 const router = require("./routes");
 app.use("/api", router);
@@ -18,9 +17,9 @@ app.use("/api", router);
 const port = process.env.PORT || 5000;
 
 async function startServer() {
-    await connectToMongoDB();
-    app.listen(port, () => {
-        console.log(`Server is listening on http://localhost:${port}`);
-    });
+  await connectToMongoDB();
+  app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+  });
 }
 startServer();
